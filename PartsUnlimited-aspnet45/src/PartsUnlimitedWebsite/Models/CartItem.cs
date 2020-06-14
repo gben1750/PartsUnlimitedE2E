b7c0ed40c04cc4c5d0505deb882493298b66b844
gben1750 <gbenson21a@bigpond.com>
2020-06-14 18:23:55 +1000

    Committing a staged change to CartItem.cs

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,8 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+            // Comment @gben1750 @ 6.17pm on June 12 2020
+            // Now adding a second commit @ 6.21pm on June 12 2020
         [Key]
         public int CartItemId { get; set; }
 
